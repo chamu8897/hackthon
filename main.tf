01b255063b6a6936b5fe40e81adc24966dfc6318
chamu93 <chamulekkala93@gmail.com>
2026-02-20 15:25:00 +0530

    VPC

--- main.tf
+++ main.tf
@@ -180,13 +180,4 @@ resource "aws_ecs_service" "fargate_service" {
   }
 }
 
-resource "aws_db_subnet_group" "mysql_subnet_group" {
-  name       = "mysql-subnet-group"
-  subnet_ids = var.subnet_ids
-
-  tags = {
-    Name = "mysql-subnet-group"
-  }
-}
-
 

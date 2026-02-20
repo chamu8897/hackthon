2f30d9adada232ae262542adf024c4b42b782b23
chamu93 <chamulekkala93@gmail.com>
2026-02-20 15:38:16 +0530

    VPC

--- main.tf
+++ main.tf
@@ -121,26 +121,6 @@ resource "aws_ecs_cluster" "fargate_cluster" {
   name = "fargate-cluster"
 }
 
-# Task Execution Role
-resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecsTaskExecutionRole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Action = "sts:AssumeRole",
-      Principal = { Service = "ecs-tasks.amazonaws.com" },
-      Effect = "Allow",
-      Sid    = ""
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
-  role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
 # ECS Task Definition
 resource "aws_ecs_task_definition" "fargate_task" {
   family                   = "fargate-task"
@@ -178,6 +158,9 @@ resource "aws_ecs_service" "fargate_service" {
     security_groups = [aws_security_group.ecs_sg.id]
     assign_public_ip = true
   }
+}
+data "aws_iam_role" "execution_role_arn" {
+  name = "ecsTaskExecutionRole"
 }
 
 

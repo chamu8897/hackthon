fe131807717d43dd27d2137d10de5b64eff068ac
chamu93 <chamulekkala93@gmail.com>
2026-02-20 17:55:32 +0530

    docker image

--- main.tf
+++ main.tf
@@ -206,7 +206,7 @@ resource "aws_ecs_task_definition" "task2" {
   container_definitions = jsonencode([
     {
       name      = var.task2_name
-      image     = 111225938018.dkr.ecr.eu-west-1.amazonaws.com/chamu-patient-serv:latest
+      image     = "111225938018.dkr.ecr.eu-west-1.amazonaws.com/chamu-patient-serv:latest"
       essential = true
       portMappings = [
         {
